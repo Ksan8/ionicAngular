bbb6e13abd3a9dc6e44f2963e75a8a661caf2e2a
Kezia Wineberg <Ksan8@protonmail.com>
2021-03-26 14:18:25 -0700

    Add modal, explanatory text, and links

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ModalComponent } from './modal/modal.component';
+import { IonicModule } from '@ionic/angular';
+
+@NgModule({
+  declarations: [ModalComponent],
+  imports: [
+    CommonModule,
+    IonicModule,
+  ],
+  exports: [
+    ModalComponent
+  ]
+})
+export class ComponentsModule { }
--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ModalController } from '@ionic/angular';
+import { ModalComponent } from '../components/modal/modal.component';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,12 +10,20 @@ import { Router } from '@angular/router';
 })
 export class DashboardPage implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private modalCtrl: ModalController) { }
 
   ngOnInit() {
   }
 
   onSubmit(): void {
     this.router.navigate(['/account']);
+  }
+
+  async showModal(): Promise<any> {
+    const modal = await this.modalCtrl.create({
+      component: ModalComponent,
+    });
+
+    return await modal.present();
   }
 }
